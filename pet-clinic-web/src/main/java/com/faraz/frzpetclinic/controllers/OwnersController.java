4896f84de882f848f916b43223ae06a6921e4633
Faraz Noor <faraznoor75@gmail.com>
2021-06-28 15:51:51 +0530

    Create a Owner Index Page and Controller. closes #20

---
+++ pet-clinic-web/src/main/java/com/faraz/frzpetclinic/controllers/OwnersController.java
@@ -0,0 +1,12 @@
+package com.faraz.frzpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RequestMapping("/owners")
+@Controller
+public class OwnersController {
+    @RequestMapping({"/","","/index","/index.html"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
