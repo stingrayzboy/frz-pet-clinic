7406c8ecdcf66e67abce8bd2b49a62c5ba4c0f97
Faraz Noor <faraznoor75@gmail.com>
2021-08-08 14:29:29 +0530

    Addded Spring Config. Closes #21

--- pet-clinic-web/src/main/java/com/faraz/frzpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/com/faraz/frzpetclinic/controllers/OwnersController.java
@@ -1,12 +1,23 @@
 package com.faraz.frzpetclinic.controllers;
 
+import com.faraz.frzpetclinic.services.OwnerService;
+import com.faraz.frzpetclinic.services.map.OwnerServiceMap;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 @Controller
 public class OwnersController {
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/","","/index","/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
