e3b3190742cdb9cfe103e7d495e6a84c800854d0
Faraz Noor <faraznoor75@gmail.com>
2021-08-08 16:28:52 +0530

    Refactored Maps to autogenerate ID

--- pet-clinic-web/src/main/java/com/faraz/frzpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/faraz/frzpetclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Faraz");
         owner1.setLastName("Noor");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Maria");
         owner2.setLastName("Noor");
 
@@ -38,13 +36,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Gajodhar");
         vet1.setLastName("Chauhan");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Ram");
         vet2.setLastName("Dayal");
 
